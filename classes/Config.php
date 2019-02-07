c90979ac915ada5a69c961ebc0118171aeeeb435
Aleksandar Dobrinić <aleksandar.dobrinic@predavaci.algebra.hr>
2019-02-07 20:04:02 +0100

    Created config system

---
+++ classes/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+class Config{
+
+    public static function get($path = null){
+
+        if ($path) {
+            $items = require_once 'config/' . $path . '.php';
+            return $items;
+        }
+        return false;
+    }
+}
+
+?>
--- index.php
+++ index.php
@@ -11,7 +11,7 @@ Helper::getHeader('Algebra Contacts', 'main-header');
 		<div class="col-xs-12 col-md-8 col-lg-6 col-md-offset-2 col-lg-offset-3">
 			<div class="jumbotron">
 				<div class="container">
-					<h1>Algebra Auth</h1>
+					<h1><?php echo Config::get('app')['name'] ?></h1>
 					<p>Sed at purus condimentum, dictum lacus vel, convallis metus. Phasellus lacinia, sem sed tincidunt suscipit, velit lectus placerat urna, id accumsan ante nisi ut metus. Ut a justo sed nibh egestas pellentesque. Aenean tempor, sapien vel egestas bibendum, dolor dolor pulvinar leo, et euismod mauris purus nec augue. Integer vulputate vitae urna vitae porta. Morbi semper purus purus, vitae ultricies lectus consectetur eget. In turpis eros, sollicitudin sed tincidunt a, auctor vitae ex. Integer accumsan lacus ac erat pretium scelerisque. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
 					<p>
 						<a class="btn btn-primary btn-lg" href="login.php" role="button">Sign In</a>
@@ -24,6 +24,10 @@ Helper::getHeader('Algebra Contacts', 'main-header');
 	</div>
 
 <?php
+
+echo '<pre>';
+print_r(Config::get('session'));
+echo '</pre>';
 
 Helper::getFooter();
 
